96d34ef663135e4589520bcccab2b019d79342b5
George G <ggogarten@gmail.com>
2016-06-21 12:17:29 -0400

    Initial Commit

---
+++ Downloading An Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Downloading An Image
+//
+//  Created by George Gogarten on 6/21/16.
+//  Copyright © 2016 George Gogarten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
